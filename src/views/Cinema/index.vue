6a24b5ced5c6c5d76e7a54a25c599ee9a1807f2d
myweb <1695301935@qq.com>
2020-04-21 22:30:20 +0800

    add createComponent

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div  id="main">
+     <Header  title="喵喵影院"/>
+     <TabBar />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import TabBar from '@/components/TabBar';
+export default {
+    name:'cinema',
+     components:{
+         Header,
+         TabBar
+     }
+}
+</script>
+
+<style>
+
+</style>
